9a8530e4c0976b452dd1418371e3627ea8f65bfe
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-18 19:51:29 +0300

    gereksiz dosyaları silindi ve type.ts oluşturuldu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Input from "./components/Input";
+import { useState } from "react";
+import { Todos } from "./types/Type";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todos[]>([]);
+
+  console.log(todo);
+
+  const addMessage = () => {
+    if (todo) setTodos([...todos, { message: todo, id: todos.length + 1 }]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input addMessage={addMessage} todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
